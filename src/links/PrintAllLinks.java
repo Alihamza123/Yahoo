938a0e275764e7f597b77ec36136f3e3624ad25d
Ali Hamza <AHIbrahim449@gmail.com>
2016-02-09 22:06:39 -0800

    Tried a few times to get a proper loop to work

--- src/links/PrintAllLinks.java
+++ src/links/PrintAllLinks.java
@@ -1,5 +1,6 @@
 package links;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -10,16 +11,70 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PrintAllLinks {
 
-	static WebDriver driver;
+	
 
 	public static final String webpage = "https://www.yahoo.com";
 
 	public static final int DEFAULT_WAIT_4_PAGE = 20;
+	
 
-	public static void main(String[] args) {
 
-		// load a new instance of firefox driver
-		driver = new FirefoxDriver();
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		
+		driver.get(webpage);
+		
+		Thread.sleep(3000L);
+		
+		List<WebElement> links = driver.findElements(By.xpath(".//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) ')]"));
+
+		Iterator<WebElement> iter = links.iterator();
+
+		while(iter.hasNext()) {
+		    WebElement we = iter.next();
+
+		       we.click();
+		       driver.navigate().back();
+		    }
+		
+		
+/*		Thread.sleep(3000L);
+
+		List<WebElement> allLinks =   
+				  driver.findElements(By.xpath(".//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) ')]"));
+
+		int count = allLinks.size()-1;
+		for (int i = 0 ; i<count ; i++){
+			allLinks.get(i).click();
+			System.out.println(driver.getTitle());
+			driver.navigate().back();
+		}*/
+		 
+				 
+/*		driver.get(webpage);
+		List<WebElement> linksize = driver.findElements(By.xpath(".//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) ')]")); 
+		linksCount = linksize.size();
+		System.out.println("Total no of links Available: "+linksCount);
+		links= new String[linksCount];
+		System.out.println("List of links Available: ");  
+		// print all the links from webpage 
+		for(int i=0;i<linksCount;i++)
+		{
+		links[i] = linksize.get(i).getAttribute("href");
+		System.out.println(all_links_webpage.get(i).getAttribute("href"));
+		} 
+		// navigate to each Link on the webpage
+		for(int i=0;i<linksCount;i++)
+		{
+		driver.navigate().to(links[i]);
+		Thread.sleep(3000);
+		}*/
+		
+	/*	// load a new instance of firefox driver
+		WebDriver driver = new FirefoxDriver();
 
 		// add initial implicit wait
 		driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS);
@@ -29,17 +84,24 @@ public class PrintAllLinks {
 
 		// navigate to www.yahoo.com
 		driver.get(webpage);
+		
+		Thread.sleep(3000L);
 
 		// save left links to list 
-		List <WebElement> allLinks = driver.findElements(By.xpath(".//span[contains(@id,'yui_3_18_0_4_1454981472699')]"));
+		List <WebElement> allLinks = driver.findElements(By.xpath(".//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) ')]"));
+		
+		int linkCount = allLinks.size();
 		
 		// print links info and click 
-		for(int i = 0 ; i <= allLinks.size() ; i++){
-			System.out.println(" Name of Link #"+i+allLinks.get(i).getText());
-			System.out.println(" URL of Link #"+i+allLinks.get(i).getAttribute("href"));
-			//allLinks.get(i).click();
-			//System.out.println("Current Page Title : " + driver.getTitle());
-		}
+		for(int i = 0 ; i <= linkCount ; i++){
+		
+			allLinks.get(i).click();
+			System.out.println(driver.getTitle());
+			driver.navigate().back();
+		
+		}*/
+		//.//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) D(tbc) Fz(14px')]
+		//.//span[contains(@id,'yui_3_18_0_4_1454981472699')][contains(@class,'Va(m) ')]
 	}
 
 }
